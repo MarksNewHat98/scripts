0946eab5e0a45b32d85a03f628f93ba84734a960
MarksNewHat98 <93878714+MarksNewHat98@users.noreply.github.com>
2026-05-31 16:52:12 +0000

    Create LoadstringPatcher.lua

---
+++ LoadstringPatcher.lua
@@ -0,0 +1,14 @@
+-- uses lua regex: https://gitspartv.github.io/lua-patterns/
+loadstring(game:HttpGet("https://github.com/MarksNewHat98/scripts/raw/refs/heads/main/LoadstringPatcherSrc.lua", true))({
+    -- example patches
+    [{ "example_code", '(print%(")(this string is unique)("%))'}] = "%1%2%2%3", -- repeats the string "this string is unique" in the print statement
+    [{ { "blacklist", "example_code" }, "(print%()'haha cant inject here'(%))", "%1[INJECTED_FUNCTION]%(%)%2" }] = function()
+        return "hello we have injected"
+    end, -- replaces the print's string with a function
+})
+-- insert your loadstring script to be patched down here
+
+--[[
+local example_code = "print("this string is unique")\nprint("this string is here")\nloadstring("print('haha cant inject here')")()"
+loadstring(example_code, "example_code")()
+--]]
