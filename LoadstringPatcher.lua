c2fceae05addd3aef52b61aaa594d66994f46871
MarksNewHat98 <93878714+MarksNewHat98@users.noreply.github.com>
2026-05-31 17:22:51 +0000

    fuh you roblox

--- LoadstringPatcher.lua
+++ LoadstringPatcher.lua
@@ -2,7 +2,7 @@
 loadstring(game:HttpGet("https://github.com/MarksNewHat98/scripts/raw/refs/heads/main/LoadstringPatcherSrc.lua", true))(getfenv(), {
     -- example patches
     [{ "example_code", '(print%(")(this string is unique)("%))'}] = "%1%2%2%3", -- repeats the string "this string is unique" in the print statement
-    [{ { "blacklist", "example_code" }, "(print%()'haha cant inject here'(%))", "%1[INJECTED_FUNCTION]%(%)%2" }] = function()
+    [{ { "blacklist", "example_code" }, "(print%()'haha cant inject here'(%))", "%1[INJECTED_FUNCTION]()%2" }] = function()
         return "hello we have injected"
     end, -- replaces the print's string with a function
 })
